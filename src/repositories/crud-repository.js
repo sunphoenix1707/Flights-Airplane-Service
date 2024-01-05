6133b5be7e64b0ce84a503a58292aa2fd029fe2a
Sunpreet Kaur <ssunpreet555@gmail.com>
2024-01-05 15:13:18 +0530

    FlightsAirplaneService

--- src/repositories/crud-repository.js
+++ src/repositories/crud-repository.js
@@ -11,48 +11,35 @@ class CrudRepository {
        
     }
     async destroy(data) {
-        try {
+      
             const response = await this.model.desroy({
                 where: {
                     id: data
                 }
             });
             return response;
-        } catch(error) {
-            Logger.error(`Something went wrong in the crud repository : destroy`);
-            throw error;
-        }
+        
     }
     async get(data) {
-        try {
+       
             const response = await this.model.findByPk(data);
             return response;
-        } catch(error) {
-            Logger.error(`Something went wrong in the crud repository : get`);
-            throw error;
-        }
+       
     }
     async getall() {
-        try {
             const response = await this.model.findall();
             return response;
-        } catch(error) {
-            Logger.error(`Something went wrong in the crud repository : getall`);
-            throw error;
-        }
+       
     }
     async update(id , data) {
-        try {
+        
             const model = await this.model.update(data , {
                 where: {
                     id:id
                 }
             });
             return response;
-        } catch(error) {
-            Logger.error(`Something went wrong in the crud repository : create`);
-            throw error;
-        }
+        
     }
 }
 module.exports = CrudRepository;
--- src/services/airplane-service.js
+++ src/services/airplane-service.js
@@ -19,7 +19,16 @@ async function createAirplane(data) {
             throw new AppError('Cannot create a new Airplance object', StatusCodes.INTERNAL_SERVER_ERROR);
         }
     }
+    async function getAirplanes() {
+        try {
+            const airplanes = await airplaneRepository.getAll();
+            return airplanes;
+        } catch(error) {
+            throw new AppError('Cannot fetch data of all the airplanes', StatusCodes.INTERNAL_SERVER_ERROR);
+        }
+    }
 
 module.exports = {
-    createAirplane
+    createAirplane,
+    getAirplanes
 }
