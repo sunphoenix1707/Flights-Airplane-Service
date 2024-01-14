1fc22b29498538717bd70141abd5e119abc8e273
sun-phoenix <114605699+sunphoenix1707@users.noreply.github.com>
2024-01-14 15:05:54 +0530

    added proxy

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use('/api' , apiRoutes);
+app.use('/flightsService/api', apiRoutes);
 app.listen(ServerConfig.PORT , () => {
     console.log(`successfully completed on port ${ServerConfig.PORT}`);
     Logger.info("successfully started the server"  , {});
